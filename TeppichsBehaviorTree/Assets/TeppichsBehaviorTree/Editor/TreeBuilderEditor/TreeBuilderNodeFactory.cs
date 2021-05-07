34e8058bd8dedeb89b3e7335f85d6453b48880a7
teppich <johannesspicker@me.com>
2021-05-08 01:14:40 +0200

    new tree builder and some reflection shenanigans

---
+++ TeppichsBehaviorTree/Assets/TeppichsBehaviorTree/Editor/TreeBuilderEditor/TreeBuilderNodeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace TeppichsBehaviorTree.Editor.Tutorial
+{
+    public class TreeBuilderNodeFactory
+    {
+        private class MyClass
+        {
+            public string someString;
+
+            public MyClass(string someString, int someInt, float someFloat, Vector3 herrVector) { this.someString = someString; }
+        }
+
+        //eine dataNode hat
+        //"blackboard" mit 
+        //type to name to value dictionaries
+
+        public static void SomeFunc()
+        {
+            var ctors = typeof(MyClass).GetConstructors();
+
+            var ctor = ctors[0];
+
+            foreach (var param in ctor.GetParameters())
+            {
+                Debug.Log(param.Position.ToString());
+                Debug.Log(param.Name);
+                Debug.Log(param.ParameterType);
+            }
+        }
+    }
+}
